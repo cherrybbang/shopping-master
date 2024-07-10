448fea120df7e129af597320ed090dec4a3675b3
JiYu <146607445+cherrybbang@users.noreply.github.com>
2024-07-10 18:29:39 +0900

    헤더 푸터 작업

---
+++ front/front/src/components/FooterPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="wrap">
+        <div class="btn_group">
+            <a href="#">홈</a>
+            <a href="#">카테고리</a>
+            <a href="#">좋아요</a>
+            <a href="#">마이페이지</a>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+* {
+    margin: 0; padding: 0;
+    box-sizing: border-box;
+    font-size: 1rem;
+    text-decoration: none;
+    color: #000;
+}
+.wrap {
+    width: 100%; height: 50px;
+    border: 1px solid red;
+}
+.btn_group {
+    display: flex;
+    justify-content: space-around;
+}
+.btn_group > a {
+    line-height: 50px;
+    border: 1px solid orange;
+}
+</style>
---
+++ front/front/src/components/HeaderPage.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="wrap">
+        <div class="header_group">
+            <div class="logo">
+                <a href="#">로고</a>
+            </div>
+
+            <div class="nav">
+                <input type="text" placeholder="검색창">
+                <a href="#">장바구니</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+* {
+    margin: 0; padding: 0;
+    box-sizing: border-box;
+    font-size: 1rem;
+    text-decoration: none;
+    color: #000;
+}
+.wrap {
+    width: 100%; height: 50px;
+    border: 1px solid red;
+}
+.header_group {
+    display: flex;
+    justify-content: space-between;
+    width: inherit; height: inherit;
+    border: 1px solid blue;
+}
+.logo {
+    width: 100px; height: 50px;
+    border: 1px solid orange;
+}
+.nav {
+    width: 300px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid orange;
+}
+.nav > input {
+    width: 70%; height: 30px;
+}
+</style>
